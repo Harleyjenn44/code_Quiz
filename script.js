3292f6eba82c75ccec78f85b019b5a59541ecd88
Ashton Beaudoin <ashtonbeaudoin2000@hotmail.com>
2020-01-06 18:35:39 -0600

    added content

---
+++ script.js
@@ -0,0 +1,61 @@
+const start = document.getElementById("content")
+
+var startBtn = document.createElement("button")
+
+startBtn.textContent = "Start Quiz"
+
+start.appendChild(startBtn)
+
+startBtn = document.addEventListener("click", )
+
+
+
+
+
+
+// var questions = [
+//     {
+//         question: "What does CSS stand for?",
+//        answers: {
+//            a: 'circling style sheet',
+//            b: 'cascading styles sheets',
+//            c: 'cascading style sheets'
+//        },
+//        correctAnswer: 'c'
+//    }
+//    {
+//        question: "What is the purpose of an HTML doc?",
+//        answers: {
+//            a: 'It allows us to style the webpage.',,
+//            c: 'It is the basic document needed to display your webpage online.'
+//        },
+//        correctAnswer: 'c'
+//    }
+//    {
+//        question: "What is the purpose of a Stylesheet?",
+//        answers: {
+//            a: 'It allows us to style the webpage.',
+//            b: 'It allows us to animate the webpage.',
+//            c: 'It is the basic document needed to display your webpage online.'
+//         },
+//        correctAnswer: 'a'
+//    }
+//    {
+//        question: "What does BootStrap allow us to do?",
+//        answers: {
+//            a: 'It allows us to customize a webpage faster and easier.',
+//        b: 'It allows us to style the webpage',
+//        c: 'It allows us to animate the webpage'
+//        },
+//        correctAnswer: 'a'
+//    }
+//    {
+//        question: "Bootstrap is to CSS as JQuery is to?",
+//        answers:
+//            a: 'HTML',
+//         b: 'Java',
+//        c: 'JavaScript'
+//        },
+//   correctAnswer: 'c'
+//     }
+// ]
